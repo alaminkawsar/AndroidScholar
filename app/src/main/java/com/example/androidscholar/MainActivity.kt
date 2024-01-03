ea3b0e0b4672733464366a287188895ef8b7d0b5
Md. Alamin Kawsar <akawsar@mymedicalhub.com>
2024-01-03 20:45:04 +0600

    added: - custom toast message - design layout with xml

--- app/src/main/java/com/example/androidscholar/MainActivity.kt
+++ app/src/main/java/com/example/androidscholar/MainActivity.kt
@@ -1,46 +1,28 @@
 package com.example.androidscholar
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidscholar.ui.theme.AndroidScholarTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var btnStart: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AndroidScholarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.layout)
+        btnStart = findViewById(R.id.show_button)
+
+        btnStart.setOnClickListener { onClick(it) }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidScholarTheme {
-        Greeting("Android")
+    private fun onClick(view: View?) {
+        Log.v("MainActivity", "Button Clicked ${view?.id==R.id.show_button}")
+        val customView = layoutInflater.inflate(R.layout.customtoast_layout, findViewById(R.id.custom_toastId))
+        val toast = Toast(applicationContext)
+        toast.duration = Toast.LENGTH_LONG
+        toast.view = customView
+        toast.show()
     }
 }
