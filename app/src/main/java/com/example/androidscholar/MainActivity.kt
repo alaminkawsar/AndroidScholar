ddc75c3e5d5392f1fb385c174ed65a702cfb3dd5
Al Amin Kawsar <akawsar@mymedicalhub.com>
2024-11-25 21:26:20 +0600

    initiate

--- app/src/main/java/com/example/androidscholar/MainActivity.kt
+++ app/src/main/java/com/example/androidscholar/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidscholar.ui.theme.AndroidScholarTheme
 
-class MainActivity : ComponentActivity() {
+class GotoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
